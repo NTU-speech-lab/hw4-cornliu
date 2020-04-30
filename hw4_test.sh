180d4975c5f88e77886da970efd631683b180846
cornliu <59017638+cornliu@users.noreply.github.com>
2020-04-30 20:31:26 +0800

    Add files via upload

---
+++ hw4_test.sh
@@ -0,0 +1,3 @@
+wget https://github.com/NTU-speech-lab/hw4-cornliu/releases/download/untagged-963936b2fb0e2ddc3e8d/ckpt.model
+wget https://github.com/NTU-speech-lab/hw4-cornliu/releases/download/untagged-963936b2fb0e2ddc3e8d/w2v_all.model
+python3 code/hw4_test.py $1 $2
---
+++ hw4_train.sh
@@ -0,0 +1,1 @@
+python3 code/hw4_train.py $1 $2
